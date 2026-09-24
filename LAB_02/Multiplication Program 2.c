5051e64092571bcbf7f5eefe8a2953ad0ad4f2ca
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 11:10:39 -0700

    Multiplication Program answer in terms of Product

---
+++ LAB_02/Multiplication Program 2.c
@@ -0,0 +1,15 @@
+/*Multiplication Program 2*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=2;
+    b=4;
+    c=6;
+    d=8;
+    e=10;
+    int f;
+    f=a*b*c*d*e;
+    printf("Product of %d,%d,%d,%d and %d is %d",a,b,c,d,e,f);
+    return 0;
+}
