f6404272dd37f40d3d2beb9f052d2fc76aa62a21
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 11:52:47 -0700

    Modulus Program direct answer

---
+++ LAB_02/Modulus Program 1.c
@@ -0,0 +1,15 @@
+/*Modulus Program 1*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=10;
+    b=17;
+    c=25;
+    d=8;
+    e=3;
+    int f;
+    f=a % b % c % d % e;
+    printf("%d",f);
+    return 0;
+}
