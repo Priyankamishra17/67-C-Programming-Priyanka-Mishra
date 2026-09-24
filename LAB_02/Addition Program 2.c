f0cc65e7756987a375d6cc9953e6a9e3a5a7b0d7
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 10:10:15 -0700

    Addition Program answer in terms of Sum

---
+++ LAB_02/Addition Program 2.c
@@ -0,0 +1,15 @@
+/*Addition Program 2*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=20;
+    b=40;
+    c=60;
+    d=80;
+    e=100;
+    int f;
+    f=a+b+c+d+e;
+    printf("Sum of %d,%d.%d,%d and %d is %d",a,b,c,d,e,f);
+    return 0;
+}
