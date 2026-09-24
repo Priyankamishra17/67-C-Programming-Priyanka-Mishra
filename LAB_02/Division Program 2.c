d27e4764357033967841e87b969dfd4a5d1809e3
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 11:30:45 -0700

    Division Program answer in terms of Quotient

---
+++ LAB_02/Division Program 2.c
@@ -0,0 +1,15 @@
+/*Division Program 2*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=2000;
+    b=2;
+    c=5;
+    d=4;
+    e=25;
+    int f;
+    f=a/b/c/d/e;
+    printf("Quotient of %d,%d,%d,%d and %d is %d",a,b,c,d,e,f);
+    return 0;
+}
