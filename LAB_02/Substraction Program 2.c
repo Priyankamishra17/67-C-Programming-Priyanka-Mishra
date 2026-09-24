22846fb8184252c50f6bab12197ac4b49a4d2fd3
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 10:24:14 -0700

    Substraction Program using the term Difference

---
+++ LAB_02/Substraction Program 2.c
@@ -0,0 +1,15 @@
+/*Substraction Program 2*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=2000;
+    b=400;
+    c=600;
+    d=80;
+    e=20;
+    int f;
+    f=a-b-c-d-e;
+    printf("Difference of %d,%d.%d,%d and %d is %d",a,b,c,d,e,f);
+    return 0;
+}
