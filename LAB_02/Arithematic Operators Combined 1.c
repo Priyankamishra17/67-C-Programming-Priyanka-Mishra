dd28025e415e1a7c75230970bb96ed74977ad261
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 12:10:18 -0700

    Arithematic Operations Combined direct answer

---
+++ LAB_02/Arithematic Operators Combined 1.c
@@ -0,0 +1,19 @@
+/*Arithematic Operations Combined 1*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,result;
+    a=20;
+    b=10;
+    result = a+b;
+    printf("%d",result);
+    result = a-b;
+    printf("\n%d",result);
+    result = a*b;
+    printf("\n%d",result);
+    result = a/b;
+    printf("\n%d",result);
+    result = a % b;
+    printf("\n%d",result);
+    return 0;
+}
