3e6644d086681a8e4ac242947500999daa971792
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 10:21:27 -0700

    Substraction Program direct answer

---
+++ LAB_02/Substraction Program 1.c
@@ -0,0 +1,15 @@
+/*Substraction Program 1*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=200;
+    b=4;
+    c=10;
+    d=12;
+    e=64;
+    int f;
+    f=a-b-c-d-e;
+    printf("%d",f);
+    return 0;
+}
