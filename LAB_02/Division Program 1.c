858b0427a68f2742eba8a3e4c8e24d81d632fe76
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 11:28:30 -0700

    Division Program direct answer

---
+++ LAB_02/Division Program 1.c
@@ -0,0 +1,15 @@
+/*Division Program 1*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=20000;
+    b=20;
+    c=5;
+    d=4;
+    e=2;
+    int f;
+    f=a/b/c/d/e;
+    printf("%d",f);
+    return 0;
+}
