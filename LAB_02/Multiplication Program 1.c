6215a21f166baeca3de11af8e19d88d36041ad98
Priyanka Mishra <priyankamishraflieshigh@gmail.com>
2026-09-24 10:44:41 -0700

    Multiplication Program direct answer

---
+++ LAB_02/Multiplication Program 1.c
@@ -0,0 +1,15 @@
+/*Multiplication Program 1*/
+#include <stdio.h>
+int main ()
+{
+    int a,b,c,d,e;
+    a=1;
+    b=2;
+    c=3;
+    d=4;
+    e=5;
+    int f;
+    f=a*b*c*d*e;
+    printf("%d",f);
+    return 0;
+}
